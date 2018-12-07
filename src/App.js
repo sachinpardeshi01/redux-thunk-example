76f877684165d2a8c48e05fb62b495283aa20d6c
sachin@coditas <sachin.pardeshi@coditas.com>
2018-12-07 17:53:21 +0530

    async await initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data : []
+    }
+  }
+
+  async componentDidMount(){
+    try{
+    const response = await fetch(`https://api.coinmarketcap.com/v1/ticker/?limit=10`);
+    const json = await response.json();
+    this.setState({ data: json });
+    }catch(err){
+      console.log("dsds",err);
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      {this.state.data.map(el => (<li>{el.name}: {el.price_usd}</li>))}
       </div>
     );
   }
